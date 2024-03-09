482a4637dc052f2f78ce155e18e77645ab2159e9
Cory Chilton <corychilton@Corys-MacBook-Pro.local>
2024-03-09 09:57:58 -0800

    routing to pages is functional

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { write } = require('fs');
 const http = require('http');
 const fs = require('fs').promises;
 
@@ -6,8 +7,8 @@ const port = 8080;
 
 console.log(__dirname)
 
-const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
+function write_html_to_res(res, page_name) {
+    fs.readFile(__dirname + "/" + page_name)
     .then(contents => {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(contents);
@@ -16,6 +17,23 @@ const requestListener = function (req, res) {
         res.writeHead(500, {"Content-Type": "text/plain"});
         res.end(err.message);
     });
+}
+
+const requestListener = function (req, res) {
+    console.log(req.url)
+    switch(req.url) {
+        case '/':
+            write_html_to_res(res, "index.html");
+            break;
+        case '/contact-me':
+            write_html_to_res(res, "contact-me.html");
+            break;
+        case '/about':
+            write_html_to_res(res, "about.html");
+            break;
+        default:
+            write_html_to_res(res, "404.html");
+    }
 };
 
 const server = http.createServer(requestListener);
